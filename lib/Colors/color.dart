0f52515378dbd5eb7e7fa78154c32d4244f9e6aa
TsalemisIasonas <tsalemisi@gmail.com>
2024-07-17 12:44:41 +0300

    update - color - MyApp

---
+++ lib/Colors/color.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const grayColor = Color(0xff4F4F4F);
+const graydarkColor = Color(0xff1A1A1A);
+const orangeColor = Color(0xffEB7A52);
+const yellowColor = Color(0xffF5D04B);
+const creamColor = Color(0xffF6ECC9);
+const blueColor = Color(0xff9AB6DB);
+const greenColor = Color(0xffABD673);
+const blackColor = Color(0xff020202);
